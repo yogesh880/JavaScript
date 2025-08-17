129d8287ea119ef5bf2018be7be05d57b7a27e90
yogesh880 <85214154+yogesh880@users.noreply.github.com>
2025-08-18 00:03:26 +0530

    datatype & changes

---
+++ part1/changes.js
@@ -0,0 +1,6 @@
+let gameName='Spiderman';
+gameName='batman';
+console.log(gameName);
+const username = 'JohnDoe';
+// username='ram'
+console.log(username);
---
+++ part1/datatype.js
@@ -0,0 +1,21 @@
+/*
+
+String
+Number
+Boolean
+Null
+Undefined
+Symbol
+BigInt
+Object
+
+These are the basic data types in JavaScript.   
+
+*/
+// var str = "Hello, World!";
+let str = 'Hello, World!';
+var num = 42;
+var bool = true; 
+
+let user ={ name: 'John', age: 30, isStudent: false };
+       
