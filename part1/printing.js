df9558e70949a0c17aadd420d5949df6fb4e4882
yogesh880 <85214154+yogesh880@users.noreply.github.com>
2025-08-17 23:57:47 +0530

    basics output methods

---
+++ part1/printing.js
@@ -0,0 +1,8 @@
+console.log('Hello, World!');
+console.log('chai');
+
+process.stdout.write('Hello, World!');
+console.log('chai');
+
+console.table({ message: 'Hello, World!' });
+console.warn('This is a warning message');
