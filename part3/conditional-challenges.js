20d78be19c0bc8bae1310dca5e3cd22ebebea861
yogesh880 <85214154+yogesh880@users.noreply.github.com>
2025-08-24 03:17:05 +0530

    conditional-statements

---
+++ part3/conditional-challenges.js
@@ -0,0 +1,17 @@
+// checking if a number is greater than another number:
+let num1 = 10;
+let num2 = 20;
+if (num1 > num2) {
+    console.log(`${num1} is greater than ${num2}`);
+} else {
+    console.log(`${num1} is not greater than ${num2}`);
+}
+
+// checking if a string is equal to another string:
+let str1 = 'hello';
+let str2 = 'world';
+if (str1 === str2) {
+    console.log(`${str1} is equal to ${str2}`);
+} else {
+    console.log(`${str1} is not equal to ${str2}`);
+}
