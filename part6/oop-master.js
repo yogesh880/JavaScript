01fcb15684a95b5ca7bc7b4630b030d3514fe867
yogesh880 <85214154+yogesh880@users.noreply.github.com>
2025-09-04 21:45:35 +0530

    prototypes & oops

--- part6/oop-master.js
+++ part6/oop-master.js
@@ -24,7 +24,18 @@ function Animal( Type){
 Animal.prototype.Speak =function(){
     return `${this.Type} Makes a sound`;
 }
-
+function Animal(){}
+Animal.prototype.speak= function(){
+        return `Animal speaking`;
+    }  
+      
+function Dog() {}
+Dog.prototype=Object.create(Animal.prototype);
+Dog.prototype.constructor=Dog;
+   
+Dog.prototype.bark=function(){
+        return`Woof!`
+    }  
 // -------- Class ------
 class Vehicle{
     constructor(make,model){
@@ -35,11 +46,56 @@ class Vehicle{
         return `${this.model} is a car from ${this.make}`;
     }
 };
+// Inheritance
 class Car extends Vehicle{
     driver(){
         return `${this.model} : This is inheritance example`
     }
 }
 let MyCar = new Car('Tata','Safari');
-console.log(MyCar.driver());
-console.log(MyCar.start());
+// console.log(MyCar.driver());
+// console.log(MyCar.start());
+
+// Incapsulation example
+
+class BankAccount{
+    #accountBalance=0;
+   Deposit( amount){
+      this.#accountBalance=this.#accountBalance+amount;
+      return `$ ${this.#accountBalance}`
+   }
+   getBalance(){
+    return `$ ${this.#accountBalance}`
+   }
+}
+let account = new BankAccount()
+account.Deposit(50000);
+// console.log(account.getBalance());
+
+// Abstraction
+
+// Polymorphism
+class Bird{
+     Fly(){
+        return `Birds can fly.....`
+     }
+}
+class Penguin extends Bird{
+       Fly(){
+        return `Penguin Cannot fly....`
+       }
+}
+let bird = new Bird()
+// console.log(bird.Fly());
+let penguin = new Penguin();
+// console.log(penguin.Fly());
+
+
+// Static keywords
+
+class CalCulator{
+    static add(a,b){
+        return a+b;
+    }
+}
+// console.log(CalCulator.add(2,6));
